bf782d40fca46194188857c25f7e61ac70f0e0d5
inasekin <nasekinid8591@yandex.ru>
2023-01-07 22:28:56 +0300

    feat: подключил бд для таблицы с reviews в users

--- taskforce/apps/users/src/app/reviews/reviews.service.ts
+++ taskforce/apps/users/src/app/reviews/reviews.service.ts
@@ -1,11 +1,11 @@
 import CreateReviewDto from './dto/create-review.dto';
-import ReviewsMemoryRepository from './reviews-memory.repository';
 import ReviewsEntity from './reviews.entity';
 import { Injectable } from '@nestjs/common';
+import ReviewsRepository from './reviews.repository';
 
 @Injectable()
 export class ReviewsService {
-  constructor(private readonly reviewsRepository: ReviewsMemoryRepository) {}
+  constructor(private readonly reviewsRepository: ReviewsRepository) {}
 
   async create(dto: CreateReviewDto) {
     const reviewEntity = new ReviewsEntity(dto);
